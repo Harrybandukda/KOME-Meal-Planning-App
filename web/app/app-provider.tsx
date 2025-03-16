aaad23416b7e2162082acf4620619b7698f3fbf4
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-03-16 00:52:52 -0400

    Logout action and persist user session

--- web/app/app-provider.tsx
+++ web/app/app-provider.tsx
@@ -4,13 +4,13 @@ import { buildURL } from '@/lib/utils'
 import { createContext, useEffect, useState } from 'react'
 
 export type AppContextType = {
-  userId: string,
+  userId: string | null,
   setUserId: Function,
   userName: string,
 }
 
 export const AppContext = createContext<AppContextType>({
-  userId: "",
+  userId: null,
   setUserId: () => {},
   userName: "",
 })
@@ -20,16 +20,31 @@ export function AppProvider({
 }: {
   children: React.ReactNode
 }) {
-  const [userId, setUserId] = useState<string>("");
+  const [userId, setUserId] = useState<string | null>(null);
   const [userName, setUserName] = useState<string>("");
 
   useEffect(() => {
+    const userId = localStorage.getItem("userId");
     if (userId) {
+      setUserId(userId);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (userId === null) {
+      return;
+    }
+
+    if (userId) {
+      localStorage.setItem("userId", userId);
+
       fetch(buildURL("/api/user/" + userId))
         .then((response) => response.json())
         .then((data) => {
           setUserName(data.full_name);
         });
+    } else {
+      localStorage.removeItem("userId");
     }
   }, [userId]);
 
---
+++ web/app/logout/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { NavBar } from "@/components/nav-bar"
+import { useEffect, useContext } from "react"
+import ErrorBoundary from "@/components/error-boundary"
+import { AppContext } from "../app-provider"
+import { useRouter } from "next/navigation"
+
+export default function LoginPage() {
+  const { setUserId } = useContext(AppContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("Logout mounted");
+    setUserId("");
+    router.push("/");
+  }, []);
+
+  return (
+    <ErrorBoundary>
+      <main className="min-h-screen bg-white">
+        <NavBar />
+      </main>
+    </ErrorBoundary>
+  )
+}
+
